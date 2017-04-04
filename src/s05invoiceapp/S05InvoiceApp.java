eeb2ae2a32a168640833ecd8a9b35f9603a092fb
alumno <alumno@LIMSFLAB04-17D.system-uap.adm>
2017-04-03 20:31:04 -0600

    second commit

--- src/s05invoiceapp/S05InvoiceApp.java
+++ src/s05invoiceapp/S05InvoiceApp.java
@@ -2,17 +2,18 @@
 package s05invoiceapp;
 
 import java.util.Scanner ; 
-
+ 
 public class S05InvoiceApp {
  public static void main (String[] arg){
     System.out.println ("Bienvenido a la aplicación Total Factura") ;
-    System.out.println() ;
+    System.out.println () ;
     Scanner sc = new Scanner (System.in) ;
     
     System.out.print ("Ingrese Subtotal:    ") ; 
     double subtotal = sc.nextDouble () ; 
-    double impuesto = subtotal  * 0.18 ;
-    double total = subtotal + impuesto ; 
+    double impuesto = subtotal  * 0.25 ;
+    double descuento = subtotal * 0.10;
+    double total = subtotal + impuesto - descuento  ; 
     
     String mensaje = "Total Factura:   " + total + "\n" ; 
     System.out.println (mensaje) ; 
